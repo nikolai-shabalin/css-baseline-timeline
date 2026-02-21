61c689b2021e484dca20b329fb495454d2c429cd
Nikolai Shabalin <dev.nikolai.shabalin@gmail.com>
2026-02-21 13:11:53 +0300

    Чинит пути

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  site: 'https://nikolai-shabalin.github.io',
+  base: '/css-baseline-timeline',
   output: 'static',
   build: {
     inlineStylesheets: 'always'
